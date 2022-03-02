9642ea6a4071e65e61007c9f84d569230883587b
elkinguerrero007 <3439@holbertonschool.com>
2022-03-02 22:05:05 +0000

    code base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" BaseModel that defines all common attributes/methods for other classes """
+from uuid import uuid4
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """Base class for all models"""
+
+    def __init__(self):
+        """ instantiates a new object
+            Args:
+                *args: variable length argument list not used
+                **kwargs: (key - value) pair of attributes
+            """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Returns a string representation of the object """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__)
+
+    def save(self):
+        """ updates the public instance attribute
+        updated_at with current time"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        new_dict = dict(self.__dict__)
+        new_dict["created_at"] = self.created_at.isoformat(sep='T')
+        new_dict["updated_at"] = self.updated_at.isoformat(sep='T')
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
