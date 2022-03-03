faeb85f6182f205486e85cff3456d641835a55eb
elkinguerrero007 <3439@holbertonschool.com>
2022-03-03 17:53:06 +0000

    up console

--- console.py
+++ console.py
@@ -58,3 +58,55 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, args):
         """End Of File command to exit the program"""
         quit()
+        
+ def emptyline(self):
+        """Do nothing on empty line\n"""
+        pass
+
+    def do_create(self, args):
+        """Creates a new instance of BaseModel \
+saves it (to the JSON file) and prints the id.
+        """
+        args = shlex.split(args)
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in my_classes:
+            print("** class doesn't exist **")
+            return
+        if args[0] in my_classes:
+            """if the args[0] is in my_classes, then the class exists"""
+            new_object = eval(args[0])()
+            new_object.save()
+            """save() saves the changes in the JSON file"""
+            print(new_object.id)
+            """print the id of the object"""
+
+    def do_show(self, args):
+        """Prints the string representation of an instance, format:
+        show <class name> <id>."""
+
+        args_list = shlex.split(args)
+        """args_list is a list of arguments passed to the command
+              shlex is a lexical analyser for simple shell-like syntax;
+                and shlex.split() splits a string into a list of tokens."""
+        if not args:
+            print("** class name missing **")
+            return
+        elif args_list[0] not in my_classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args_list) == 1:
+            print("** instance id missing **")
+            return
+        new_object = "{}.{}".format(args_list[0], args_list[1])
+        """new_object is a string that is the class name an the id"""
+        if new_object not in models.storage.all().keys():
+            """if the new_object is not in the dictionary,
+            then the object doesn't exist"""
+            print("** no instance found **")
+            return
+        else:
+            print("[{}] ({}) {}".format(args_list[0], new_object[1],
+                  models.storage.all()[new_object]))
+            """print the object in format [class name] (id) object"""
