557f518b6dcc66503c70f7c23cb88bb6cc2f1a31
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-03-05 19:57:15 -0500

    Update test_state.py
    new act file code

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,47 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Unitest for models/state.py
-
-Unittest classes:
-    test_state_instantiates
-    test_state_save
-    test_state_dict
-    """
-
+    This is a module test from BaseModel class and your methods.
+"""
 import unittest
-import models
 from models.state import State
 
 
-class test_state_instantiates(unittest.TestCase):
-    """ Unittest for testing instantiation"""
+class TestState(unittest.TestCase):
+    """
+    this class test user class and your behavior
+    """
 
-    def test_instantiation(self):
-        self.assertIs(State, type(State()))
+    def setUp(self):
+        self.state = State()
 
-    def test_instantiation_with_kwargs(self):
-        self.assertIs(State, type(State(name="California")))
-
-
-class test_state_save(unittest.TestCase):
-    """ Unittest for testing save"""
-
-    def test_save(self):
-        state = State()
-        state.save()
-        self.assertNotEqual(state.created_at, state.updated_at)
-
-    def test_save_updated(self):
-        state = State()
-        state.save()
-        state.save()
-        self.assertNotEqual(state.created_at, state.updated_at)
-
-    def test_save_to_json(self):
-        state = State()
-        state.save()
-        self.assertIs(type(state.to_dict()), dict)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_creation(self):
+        '''this test validate that creation proccess was correct.
+        '''
+        self.assertEqual(self.State.name, '')
